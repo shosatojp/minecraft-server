9cf008293765b95dc8028b53a2b2237120c28f95
shosatojp <me@shosato.jp>
2020-09-12 15:38:42 +0900

    forge

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,17 @@ ARG VERSION
 RUN apk update && \
     apk add openjdk8-jre
 
-COPY ./versions.txt /
-RUN wget -O /server.jar \
-    $(cat /versions.txt | grep -E "^${VERSION} " | head -1 | awk '{print $2}')
+WORKDIR /data
+COPY ./versions.txt .
+RUN cat versions.txt | grep -E "^${VERSION} " | head -1 | awk '{print $2}' > url.txt
+RUN wget -O download.jar `cat url.txt`
+RUN if [ "$(echo `cat url.txt` | grep installer)" ];then    \
+        java -jar download.jar nogui -installServer;        \
+        mv minecraft_server.*.jar server.jar;               \
+    else                                                    \
+        mv download.jar server.jar;                         \
+    fi
 
 WORKDIR /home
-CMD java -Xms${Xms} -Xmx${Xmx} -jar /server.jar nogui
+CMD java -Xms${Xms} -Xmx${Xmx} -jar /data/server.jar nogui
 EXPOSE 25565
--- getversions.py
+++ getversions.py
@@ -1,6 +1,8 @@
+import re
 from sys import version
 import requests
 import bs4
+from requests.api import request
 
 
 def get_download_link(version: str) -> str:
@@ -17,11 +19,33 @@ def get_versions():
     versions = [e.get('id') for e
                 in doc.select('.versions div:nth-child(1) .item') if e.get('id')]
 
-    with open('versions.txt', 'wt', encoding='utf-8') as f:
-        for version in versions:
-            link = get_download_link(version)
-            if link:
+    for version in versions:
+        link = get_download_link(version)
+        if link:
+            with open('versions.txt', 'at', encoding='utf-8') as f:
                 f.write(f'{version} {link}\n')
 
 
+def get_forge_version(version: str):
+    html = requests.get(f'http://files.minecraftforge.net/maven/net/minecraftforge/forge/index_{version}.html').text
+    doc = bs4.BeautifulSoup(html, 'html.parser')
+    latest = doc.select_one('.link > a[title=Installer]')
+    if latest:
+        direct = re.match('.*url=(.*)$', latest.get('href'))[1]
+        return direct
+
+
+def get_forge_versions():
+    html = requests.get('http://files.minecraftforge.net/').text
+    doc = bs4.BeautifulSoup(html, 'html.parser')
+    versions = [e.text.strip() for e
+                in doc.select('.li-version-list li')]
+    for version in versions:
+        link = get_forge_version(version)
+        if link:
+            with open('versions.txt', 'at', encoding='utf-8') as f:
+                f.write(f'forge-{version} {link}\n')
+
+
 get_versions()
+get_forge_versions()
